80fa6e8345ca232a9c5b0109da80bdafc970f497
Aleksandra Baibik <abaibik@gmail.com>
2022-12-25 14:31:50 +0100

    first test, button has correct initial color

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <button className="styled" style={{ backgroundColor: "red" }}>
+        Change to blue
+      </button>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/App.test.js
@@ -0,0 +1,15 @@
+import { render, screen, fireEvent } from "@testing-library/react";
+import App from "./App";
+
+test("button has correct initial color", () => {
+  render(<App />);
+  const button = screen.getByRole("button", { name: "Change to blue" });
+  expect(button).toHaveStyle({ backgroundColor: "red" });
+});
+
+test("button turns blue when clicked", () => {
+  render(<App />);
+  const button = screen.getByRole("button", { name: "Change to blue" });
+  fireEvent.click(button);
+  expect(button).toHaveStyle({ backgroundColor: "blue" });
+});
